f20baa684638cb2981ae8833485639f806e39d4c
Kevin Warrington <kevin.warrington@gmail.com>
2014-08-05 21:40:53 -0600

    setup docroot

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,1 +1,9 @@
-class { 'apache': }
+class { 'apache':
+	# prevent creating of default site
+	default_vhost => false,
+}
+
+apache::vhost { 'localhost':
+  port    => '80',
+  docroot => '/vagrant/htdocs',
+}
