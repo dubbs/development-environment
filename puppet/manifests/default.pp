894ea6499952ffeed3289cc162560a8571c12267
Kevin Warrington <kevin.warrington@gmail.com>
2014-08-10 20:00:03 -0600

    add php

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,11 +1,23 @@
+
+class { 'apt':
+  # run apt-get update
+  always_apt_update => true
+}
+
 # like `include apache` but allows configuration
 class { 'apache':
-	# do not want default vhost to hijack requests, create another
-  default_vhost        => false,
+  # do not want default vhost to hijack requests, create another
+  default_vhost => false,
+  # instead of `worker`, required for mod_php
+  mpm_module    => 'prefork',
+  # after apt
+  require       => Class['apt']
 }
 
 apache::vhost { 'default-dev':
-  port    => '80',
-  docroot => '/vagrant/public_html',
-  logroot => '/vagrant/logs',
+  port              => '80',
+  docroot           => '/vagrant/public_html',
+  logroot           => '/vagrant/logs',
 }
+
+include apache::mod::php
